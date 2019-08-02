c513dcf7c0cbaaefd7510d53aaf358adbf1af6ca
minjin-kim-rp <53587730+minjin-kim-rp@users.noreply.github.com>
2019-08-02 12:37:30 +0900

    [Create][beakjoon] solution of 11719
    [Desc] This commit is the solution of beakjoon 11719

---
+++ beakjoon_11719.cc
@@ -0,0 +1,27 @@
+/*
+Problem
+: Print out Input String
+Example
+-Input
+    Hello
+
+Baekjoon     
+   Online Judge 
+-Output
+    Hello
+
+Baekjoon     
+   Online Judge 
+*/
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string s;
+    while(getline(cin,s)){
+        cout<<s<<endl;
+    }
+    
+}
