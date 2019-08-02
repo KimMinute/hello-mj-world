3128c0083c93385234560d4aa14c620dc5b51692
minjin-kim-rp <53587730+minjin-kim-rp@users.noreply.github.com>
2019-08-02 11:23:12 +0900

    [Create][beakjoon] solution of 1000
    [Desc] This commit is the solution of beakjoon 1000

---
+++ beakjoon_1000.cc
@@ -0,0 +1,22 @@
+/*
+Problem
+: Input 2 integer A and B, then printout A+B
+Condition
+: 0<A,B<10
+Example
+-Input 
+1 2
+-Output
+3
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+}
+
