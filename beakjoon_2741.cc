b62b3f18af9d5997f21a8392750436b3c5b7a87b
minjin-kim-rp <53587730+minjin-kim-rp@users.noreply.github.com>
2019-08-02 16:41:24 +0900

    [Create][beakjoon] solution of 2741
    [Desc] This commit is the solution of beakjoon 2741

---
+++ beakjoon_2741.cc
@@ -0,0 +1,27 @@
+/*
+problem
+: Printout one line per line from 1 to N to given N
+Condition
+: N<=100,000
+Example
+-Input
+5
+-Output
+1
+2
+3
+4
+5
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+		cin>>N;
+    for(int i=1;i<=N;i++){
+			printf("%d\n",i);
+		}
+}
