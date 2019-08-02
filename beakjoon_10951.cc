6a76be50d44c7fd1df60ca92ab5419c6b7a1759f
minjin-kim-rp <53587730+minjin-kim-rp@users.noreply.github.com>
2019-08-02 11:48:14 +0900

    [Create][beakjoon] solution of 10951
    [Desc] This commit is the solution of beakjoon 10951

---
+++ beakjoon_10951.cc
@@ -0,0 +1,30 @@
+/*
+Problem
+: Input 2 interger A and B,then printout A+B
+Condition
+: Input is consist of several testcases. each testcase is oneline, A and B is input.
+Example
+-Input
+1 1
+2 3
+3 4
+9 8
+5 2
+-Output
+2
+5
+7
+17
+7
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    while(cin>>a>>b){
+        cout<<a+b<<endl;
+    }
+}
