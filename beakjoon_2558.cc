72a6a8766262a49d0c8af7cf75ce576a1c9fbb8d
minjin-kim-rp <53587730+minjin-kim-rp@users.noreply.github.com>
2019-08-02 11:27:37 +0900

    [Create][beakjoon] solution of 2558
    [Desc] This commit is the solution of beakjoon 2558

---
+++ beakjoon_2558.cc
@@ -0,0 +1,22 @@
+/*
+Problem
+: Input 2 integer A and B, then printout A+B
+Condition
+: Input A in first line, and B in second line. 0<A,B<10
+Example
+-Input
+1
+2
+-Output
+3
+*/
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a;
+    cin>>b;
+    cout<<a+b<<endl;
+}
