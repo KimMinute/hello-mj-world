2d3cfbf22dc37f54181ecad70fb22a065ca24f1b
minjin-kim-rp <53587730+minjin-kim-rp@users.noreply.github.com>
2019-08-03 21:53:13 +0900

    [Create][beakjoon] solution #2743
    [Desc] This commit is the solution of beakjoon #2743

---
+++ beakjoon_2743.cc
@@ -0,0 +1,16 @@
+/*
+this code is the solution of beakjoon #2743.
+please refer below url for checking problem.
+https://www.acmicpc.net/problem/2743
+*/
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(void)
+{
+	string input;
+	cin>>input;
+	cout<<input.size()<<endl;
+	
+}
