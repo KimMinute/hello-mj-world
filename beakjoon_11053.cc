8ea85962d645e789550e56690a2cde865649aa95
minjin-kim-rp <53587730+minjin-kim-rp@users.noreply.github.com>
2019-08-05 09:48:29 +0900

    [Create][beakjoon][dp] solution #11053
    [Desc] This commit is the solution of beakjoon #11053

---
+++ beakjoon_11053.cc
@@ -0,0 +1,41 @@
+/*
+this is the solution of beakjoon #11053
+you can refer below url for checking problem, condition, and example input/output
+https://www.acmicpc.net/problem/11053
+*/
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	int A[1001];
+	int D[1001];
+	int num;
+	
+	cin>>num;
+	for(int i=1;i<=num;i++){
+		cin>>A[i];
+	}
+	
+	for(int i=1;i<=num;i++){
+		D[i]=1;
+		for(int j=1;j<i;j++){
+			if(A[i]>A[j]&&D[i]<D[j]+1){
+				D[i]=D[j]+1;
+			}
+		}
+	}
+	
+	int temp_max=D[1];
+	for(int i=2;i<=num;i++){
+		if(temp_max<D[i]){
+			temp_max=D[i];
+		}
+	}	
+	
+	cout<<temp_max<<endl;
+	
+}
