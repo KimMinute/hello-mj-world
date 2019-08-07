0d2c83cbc04b59c0499b69ceec9be1ad82362af9
minjin-kim-rp <53587730+minjin-kim-rp@users.noreply.github.com>
2019-08-07 16:46:57 +0900

    [Create][beakjoon] solution #11653
    [Desc] This commit is the solution of beakjoon #11653

---
+++ beakjoon_11653.cc
@@ -0,0 +1,25 @@
+/*
+this is the solution code of #11653
+https://www.acmicpc.net/problem/11653
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int n;
+	scanf("%d",&n);
+	
+	for(int i=2;i*i<=n;i++){
+		while(n%i==0){
+			printf("%d\n",i);
+			n=n/i;
+		}
+	}
+	
+	if(n>1){
+		printf("%d\n",n);
+	}
+	
+	return 0;
+}
